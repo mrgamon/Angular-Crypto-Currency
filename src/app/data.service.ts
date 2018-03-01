aea7b2b1f7e4903be1866349b2ab2f2394a07ac1
mrgamon <mrgamon@hotmail.com>
2018-03-01 15:26:04 +0800

    modify css

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   constructor(private __http: Http) { }
 
   getPrices() {
-    return this.__http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR')
+    return this.__http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
     .map(result => this.result = result.json());
   }
 }
